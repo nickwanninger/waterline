cbb6aa729dd3ae417deb2c0521e2b40401a9f19f
Nick Wanninger <nickwanninger@gmail.com>
2023-04-07 16:22:07 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="waterline",
     packages=find_packages(),
-    version="0.1.4",
+    version="0.1.5",
     description="A unified LLVM benchmark pipeliner",
     author="Nick Wanninger",
     license="MIT",
